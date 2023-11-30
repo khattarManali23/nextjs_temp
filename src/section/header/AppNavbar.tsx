eb0c82059cc2923b4ec32978bacb00754d49d058
khattarmanali <manalikhattar1@gmail.com>
2023-11-30 22:28:15 +0530

    aded navbar

--- src/section/header/AppNavbar.tsx
+++ src/section/header/AppNavbar.tsx
@@ -62,11 +62,11 @@ const AppNavbar: React.FC = () => {
         </div>
 
         <AppDrawer open={menuVisible} onClose={toggleMenu} direction='right'>
-          <List className='flex justify-center items-center p-1'>
+          <List className='flex justify-center items-center '>
             <ListItemButton
               component='a'
               href='#'
-              className='block md:inline-block text-black hover:text-[#CF1F2F] px-1 py-3 border-b-2 text-md first-letter: font-medium md:border-none'
+              className='block md:inline-block text-black hover:text-[#CF1F2F] px-[0.1px] py-3 border-b-2 text-md first-letter: font-medium md:border-none'
             >
               <Button size='sm' variant='primary'>
                 Directory
@@ -75,7 +75,7 @@ const AppNavbar: React.FC = () => {
             <ListItemButton
               component='a'
               href='#'
-              className='block md:inline-block text-black hover:text-[#CF1F2F] px-1 py-3 border-b-2 text-md first-letter: font-medium md:border-none'
+              className='block md:inline-block text-black hover:text-[#CF1F2F] px-[0.1px] py-3 border-b-2 text-md first-letter: font-medium md:border-none'
             >
               <Button size='sm' variant='primary'>
                 Sponsorship
@@ -84,7 +84,7 @@ const AppNavbar: React.FC = () => {
             <ListItemButton
               component='a'
               href='#'
-              className='block md:inline-block text-black  px-1 py-3 border-b-2 text-md first-letter: font-medium md:border-none'
+              className='block md:inline-block text-black  px-[0.1px] py-3 border-b-2 text-md first-letter: font-medium md:border-none'
             >
               <Button size='sm' variant='primary'>
                 Contact
@@ -93,7 +93,7 @@ const AppNavbar: React.FC = () => {
             <ListItemButton
               component='a'
               href='#'
-              className='block md:inline-block text-black  px-1 py-3 border-b-2 text-md first-letter: font-medium md:border-none'
+              className='block md:inline-block text-black  px-[0.1px] py-3 border-b-2 text-md first-letter: font-medium md:border-none'
             >
               <Button size='sm' variant='primary'>
                 Sign In
