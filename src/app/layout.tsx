aa25d3667d0abea6512932245508c52ef88f1d95
khattarmanali <manalikhattar1@gmail.com>
2023-11-30 23:11:19 +0530

    aded navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    images: [`${siteConfig.url}/images/LOGO.png`],
     type: 'website',
     locale: 'en_US',
   },
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    images: [`${siteConfig.url}/images/LOGO.png`],
     // creator: '@th_clarence',
   },
   // authors: [
--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   title: 'INDIAN INSTITUDE OF  INTERIOR DESIGNERS',
   description: 'INDIAN INSTITUDE OF  INTERIOR DESIGNERS',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://iiid.vercel.app/',
 };
